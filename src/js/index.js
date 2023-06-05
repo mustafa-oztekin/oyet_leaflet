8651104839bf719b30bf112dedef73024a0f68ed
mustafa-oztekin <mustafaoztekin564@gmail.com>
2023-06-05 17:37:31 +0300

    node'ların yerleri güncellendi

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,19 @@
-var map = L.map('map').setView([(39.9286946+39.746789+40.223023)/3,
-                                (32.81890+30.474305+28.850525)/3], 9);
+var map = L.map('map').setView([39.739309, 30.483702], 12);
 
-map.on('click', function(e){
+//map.on('click', function(e){
     //debugger;
 
-    $.ajax({
-        url: "http://127.0.0.1:8000/events/",
-        method: "GET",
-        success: function(response) {
-          console.log(response);
-        },
-        error: function(error) {
-          console.log(error);
-        }
-      });
-});
+    //$.ajax({
+      //  url: "http://127.0.0.1:8000/events/",
+      //  method: "GET",
+      //  success: function(response) {
+      //    console.log(response);
+      //  },
+      //  error: function(error) {
+      //    console.log(error);
+      //  }
+    //  });
+//});
 
 var googleBasemap = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
     attribution:"Oyet",
@@ -24,9 +23,12 @@ var googleBasemap = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={
 
 googleBasemap.addTo(map);
 
-var ank = [39.9286946, 32.81890];
-var esk = [39.746789, 30.474305];
-var bur = [40.223023, 28.850525];
+var C = [39.746909, 30.474223];
+var B1 = [39.739309, 30.483702];
+var A1 = [39.733678, 30.481098];
+var A2 = [39.733149, 30.486781];
+var A3 = [39.736249, 30.489961];
+var A4 = [39.739566, 30.492043];
 
 // var nokta_ank = L.marker(ank).addTo(map);
 // var nokta_esk = L.marker(esk).addTo(map);
@@ -35,13 +37,29 @@ var bur = [40.223023, 28.850525];
 //    .bindPopup('Buradasınız.<br> Mustafa')
 //    .openPopup();
 
-var stil = {
+var stil_a = {
     color:"#c0392b",
     fill:true,
     fillColor:"#f1c40f",
     fillOpacity:0.5
 };
-stil.radius = 20000;
+stil_a.radius = 300;
+
+var stil_b = {
+  color:"#c0392b",
+  fill:true,
+  fillColor:"#f1c40f",
+  fillOpacity:0.5
+};
+stil_b.radius = 500;
+
+var stil_c = {
+  color:"#c0392b",
+  fill:true,
+  fillColor:"#f1c40f",
+  fillOpacity:0.5
+};
+stil_c.radius = 1000;
 
 var stil2 = {
     color:"#c0392b",
@@ -49,52 +67,70 @@ var stil2 = {
     fillColor:"#2ecc71",
     fillOpacity:0.6
 };
-stil2.radius = 20000;
+stil2.radius = 300;
 
-var circle_ank = L.circle(ank, stil).addTo(map);
-var circle_esk = L.circle(esk, stil).addTo(map);
-var circle_bur = L.circle(bur, stil).addTo(map);
+var circle_c = L.circle(C, stil_c).addTo(map);
+var circle_b1 = L.circle(B1, stil_b).addTo(map);
+var circle_a1 = L.circle(A1, stil_a).addTo(map);
+var circle_a2 = L.circle(A2, stil_a).addTo(map);
+var circle_a3 = L.circle(A3, stil_a).addTo(map);
+var circle_a4 = L.circle(A4, stil_a).addTo(map);
 
-var ank_zoom = circle_ank.getBounds();
-var esk_zoom = circle_esk.getBounds();
-var bur_zoom = circle_bur.getBounds();
+var c_zoom = circle_c.getBounds();
+var b1_zoom = circle_b1.getBounds();
+var a1_zoom = circle_a1.getBounds();
+var a2_zoom = circle_a2.getBounds();
+var a3_zoom = circle_a3.getBounds();
+var a4_zoom = circle_a4.getBounds();
 
 function randomNumber() {
   var num = (Math.random()*(10) + 20).toFixed(1);
   return num;
 };
 
-circle_ank.on('dblclick', function(){
-  circle_ank.bindPopup('Ankara <br> Sıcaklık: ' + randomNumber(), closeOnClick = true).openPopup();
+circle_c.on('dblclick', function(){
+  circle_c.bindPopup('C modülü <br> Sıcaklık: ' + randomNumber(), closeOnClick = true).openPopup();
+  //var sonuc = (ank[0] + esk[0]) / 2;
 });
 
-circle_esk.on('dblclick', function(){
-  circle_esk.bindPopup('Eskişehir <br> Sıcaklık: ' + randomNumber(), closeOnClick = true).openPopup();
+circle_b1.on('dblclick', function(){
+  circle_b1.bindPopup('B1 modülü <br> Sıcaklık: ' + randomNumber(), closeOnClick = true).openPopup();
 });
 
-circle_bur.on('dblclick', function(){
-  circle_bur.bindPopup('Bursa <br> Sıcaklık: ' + randomNumber(), closeOnClick = true).openPopup();
+circle_a1.on('dblclick', function(){
+  circle_a1.bindPopup('A1 modülü <br> Sıcaklık: ' + randomNumber(), closeOnClick = true).openPopup();
+});
+
+circle_a2.on('dblclick', function(){
+  circle_a2.bindPopup('A2 modülü <br> Sıcaklık: ' + randomNumber(), closeOnClick = true).openPopup();
+});
+
+circle_a3.on('dblclick', function(){
+  circle_a3.bindPopup('A3 modülü <br> Sıcaklık: ' + randomNumber(), closeOnClick = true).openPopup();
+});
+
+circle_a4.on('dblclick', function(){
+  circle_a4.bindPopup('A4 modülü <br> Sıcaklık: ' + randomNumber(), closeOnClick = true).openPopup();
 });
 
 const eventSource = new EventSource("http://127.0.0.1:8000/sse");//FastAPI SSE endpointinizin URL'si
 
 eventSource.onmessage = function(event) {
   console.log("Gelen veri:", event.data);
-  if (event.data === 'A1') {
-    map.fitBounds(ank_zoom);
-    circle_ank.setStyle(stil2);
-    setTimeout(() => {circle_ank.setStyle(stil);}, 250);
+  if (event.data === 'C') {
+    map.fitBounds(c_zoom);
+    circle_c.setStyle(stil2);
+    setTimeout(() => {circle_c.setStyle(stil);}, 250);
   }
-  else if (event.data === 'A2') {
-    map.fitBounds(esk_zoom);
-    circle_esk.setStyle(stil2);
-    setTimeout(() => {circle_esk.setStyle(stil);}, 250);
+  else if (event.data === 'B1') {
+    map.fitBounds(b1_zoom);
+    circle_b1.setStyle(stil2);
+    setTimeout(() => {circle_b1.setStyle(stil);}, 250);
   }
   else {
-    map.fitBounds(bur_zoom);
-    circle_bur.setStyle(stil2);
-    setTimeout(() => {circle_bur.setStyle(stil);}, 250);
+    map.fitBounds(a1_zoom);
+    circle_a1.setStyle(stil2);
+    setTimeout(() => {circle_a1.setStyle(stil);}, 250);
   }
   // console.log(typeof event.data);
-  // setTimeout(() => { map.fitBounds(ank_zoom); }, 2000);
 };
